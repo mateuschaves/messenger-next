963e342a2861a3c8c185304db6dff82071a4c822
Mateus Henrique <mateushenriquechaves1@gmail.com>
2023-05-16 02:11:11 -0300

    feat: middlewares

--- next.config.js
+++ next.config.js
@@ -2,16 +2,15 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    swcPlugins: ["next-superjson-plugin", {}],
+    swcPlugins: [["next-superjson-plugin", {}]]
   },
   images: {
     domains: [
-      "localhost",
-      "res.cloudinary.com",
-      "avatars.githubusercontent.com",
-      "lh3.googleusercontent.com",
-    ],
-  },
-};
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
